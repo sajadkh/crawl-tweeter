5dd46c8120a76d13ba5c6c946ab0299520ee87c6
sajad <sajadkhosravi1996@gmail.com>
2018-09-17 22:11:42 +0430

    1

--- tweeterCrawler.py
+++ tweeterCrawler.py
@@ -1,6 +1,7 @@
-import tweepy
 import csv
-import pandas as pd
+
+import tweepy
+
 ####input your credentials here
 consumer_key = 'NkuTVgTuAVQ77Ok2z4VVRt86x'
 consumer_secret = 'YxeMT2Y5bXo3NfFUPmTR9Jboaluio9G7CTzhaSOhHrL7HzY1W7'
@@ -9,19 +10,20 @@ access_token_secret = 'AzukZKxRx7yD1wOaoB02djU2ECLHckqT31sOLIhka6QX0'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth,wait_on_rate_limit=True)
+api = tweepy.API(auth, wait_on_rate_limit=True)
 
 with open('sajad.csv', 'rb') as names:
-     reader = csv.reader(names, delimiter=' ', quotechar='|')
-     for name in reader:  
-     	name.replace(" ", "_");
-		# Open/Create a file to append data
-		csvFile = open(name + '.csv', 'a')
-		#Use csv Writer
-		csvWriter = csv.writer(csvFile)
+    reader = csv.reader(names, delimiter=' ', quotechar='|')
+    for name in reader:
+        name.replace(" ", "_");
+        # Open/Create a file to append data
+        csvFile = open(name + '.csv', 'a')
+        # Use csv Writer
+        csvWriter = csv.writer(csvFile)
 
-		for tweet in tweepy.Cursor(api.search,q="#"+name,count=200,
-		                           lang="en",
-		                           since="2018-09-07", until="2018-09-17").items():
-		    #print (tweet.created_at, tweet.text, tweet.user.name, tweet.user.screen_name, tweet.user.location, tweet.retweet_count, tweet.favorite_count)
-		    csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8'), tweet.user.name, tweet.user.screen_name, tweet.user.location, tweet.retweet_count, tweet.favorite_count])
+        for tweet in tweepy.Cursor(api.search, q="#" + name, count=200,
+                                   lang="en",
+                                   since="2018-09-07", until="2018-09-17").items():
+            # print (tweet.created_at, tweet.text, tweet.user.name, tweet.user.screen_name, tweet.user.location, tweet.retweet_count, tweet.favorite_count)
+            csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8'), tweet.user.name, tweet.user.screen_name,
+                                tweet.user.location, tweet.retweet_count, tweet.favorite_count])
